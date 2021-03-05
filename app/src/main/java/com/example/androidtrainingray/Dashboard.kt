754a329bbcba179835588afeabf5c9ee9701c26e
raytumb <raytumb.design@gmail.com>
2021-03-05 15:16:22 +0700

    c4

---
+++ app/src/main/java/com/example/androidtrainingray/Dashboard.kt
@@ -0,0 +1,28 @@
+package com.example.androidtrainingray
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import okhttp3.*
+import java.io.IOException
+
+class Dashboard : AppCompatActivity() {
+
+    val client = OkHttpClient()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard3)
+
+        run( url "https://api.openweathermap.org/data/2.5/forecast?q=purwakarta,ID&appid=2ce659b9c25fc6fe3a07de4ca71d1dac")
+    }
+
+    fun run(url: String){
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, IOException) {}
+            override fun onResponse(call: Call, response: Response) = println (response.body()?.string())
+        })
+    }
+}
